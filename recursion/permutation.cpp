ceb4d752f110a94b29cb18c54a31de990067e9fb
KavyaJP <kavya31052005@gmail.com>
2025-08-06 10:49:31 +0530

    comments

--- recursion/permutation.cpp
+++ recursion/permutation.cpp
@@ -3,15 +3,21 @@ using namespace std;
 
 void permute(string s, int l, int r)
 {
+    // Base case: if left index equals right index, print the permutation
+    // which means we have a complete permutation
     if (l == r)
         cout << "{" << s << "}" << endl;
+    // Recursive case: swap each character with the leftmost character
+    // and recursively call permute for the next character
     else
     {
+        // Loop through the string from the left index to the right index
+        // swapping characters to generate permutations
         for (int i = l; i <= r; i++)
         {
-            swap(s[l], s[i]);
-            permute(s, l + 1, r);
-            swap(s[l], s[i]); // backtrack
+            swap(s[l], s[i]);     // swap the current character with the leftmost character
+            permute(s, l + 1, r); // recursively call permute for the next character
+            swap(s[l], s[i]);     // After returning from the recursive call, swap back to backtrack
         }
     }
 }
