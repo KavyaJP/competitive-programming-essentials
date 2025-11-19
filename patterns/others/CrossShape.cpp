b68861a78acc5019bf12c3fa618af0c3f9c89b34
KavyaJP <kavya31052005@gmail.com>
2025-11-19 23:02:56 +0530

    optimised the cross shape code

--- patterns/others/CrossShape.cpp
+++ patterns/others/CrossShape.cpp
@@ -6,43 +6,15 @@ int main()
     int n;
     cout << "Enter number of rows: ";
     cin >> n;
-    int half = n / 2;
-    for (int i = 0; i <= n / 2; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < half; j++)
+        for (int j = 0; j < n; j++)
         {
-            if (j == i)
+            if (j == i || j == n - i - 1)
                 cout << "* ";
             else
                 cout << "  ";
         }
-        for (int j = half; j < n; j++)
-        {
-            if (j == n - i - 1)
-                cout << "* ";
-            else
-                cout << "  ";
-        }
-        cout << endl;
+        cout << "\n";
     }
-    for (int i = (n / 2) - 1; i >= 0; i--)
-    {
-        for (int j = n - 1; j > half; j--)
-        {
-            if (j == n - i - 1)
-                cout << "* ";
-            else
-                cout << "  ";
-        }
-        for (int j = half; j >= 0; j--)
-        {
-            if (j == i)
-                cout << "* ";
-            else
-                cout << "  ";
-        }
-        cout << endl;
-    }
-
-    return 0;
 }
